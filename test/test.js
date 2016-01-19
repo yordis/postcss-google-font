0bf0c343c9b51035c771824ef0b6dd42252c4057
Yordis Prieto <yordis.prieto@gmail.com>
2016-01-19 07:51:15 -0500

    added commas to the testing

--- test/test.js
+++ test/test.js
@@ -16,50 +16,50 @@ function run(t, input, output, opts = {}) {
 
 
 test('should scape single quotes', t => {
-  const input = "@google-font 'Lato' 700"
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700)'
+  const input = "@google-font 'Lato' 700;"
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700);'
 
   return run(t, input, expectedOutput, {})
 })
 //
 test('should scape double quotes', t => {
-  const input = '@google-font "Lato" 700'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700)'
+  const input = '@google-font "Lato" 700;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700);'
 
   return run(t, input, expectedOutput, {})
 })
 
 test('should work without any quotes', t => {
-  const input = '@google-font Lato 700'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700)'
+  const input = '@google-font Lato 700;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700);'
 
   return run(t, input, expectedOutput, {})
 })
 
 test('with multiple styles', t => {
-  const input = '@google-font Lato 700,400italic'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700,400italic)'
+  const input = '@google-font Lato 700,400italic;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700,400italic);'
 
   return run(t, input, expectedOutput, {})
 })
 
 test('with subset', t => {
-  const input = '@google-font Lato 700 latin'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700&subset=latin)'
+  const input = '@google-font Lato 700 latin;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700&subset=latin);'
 
   return run(t, input, expectedOutput, {})
 })
 
 test('with multiple subset', t => {
-  const input = '@google-font Lato 700 latin,latin-ext'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700&subset=latin,latin-ext)'
+  const input = '@google-font Lato 700 latin,latin-ext;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Lato:700&subset=latin,latin-ext);'
 
   return run(t, input, expectedOutput, {})
 })
 
 test('with compound font family name', t => {
-  const input = '@google-font "Open Sans" 700'
-  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Open+Sans:700)'
+  const input = '@google-font "Open Sans" 700;'
+  const expectedOutput = '@import url(https://fonts.googleapis.com/css?family=Open+Sans:700);'
 
   return run(t, input, expectedOutput, {})
 })
